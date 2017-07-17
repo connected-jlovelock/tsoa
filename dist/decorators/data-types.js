698192750f446c8c0dfe2473ed1c15ccefefb036
Jessica and Patricio <jlovelock@connectedlab.com>
2017-07-17 16:45:59 -0400

    Commiting dist folder

---
+++ dist/decorators/data-types.js
@@ -0,0 +1,39 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function IsInt(errorMsg) {
+    return function () { return; };
+}
+exports.IsInt = IsInt;
+function IsLong(errorMsg) {
+    return function () { return; };
+}
+exports.IsLong = IsLong;
+function IsFloat(errorMsg) {
+    return function () { return; };
+}
+exports.IsFloat = IsFloat;
+function IsDouble(errorMsg) {
+    return function () { return; };
+}
+exports.IsDouble = IsDouble;
+function IsDate(errorMsg) {
+    return function () { return; };
+}
+exports.IsDate = IsDate;
+function IsDateTime(errorMsg) {
+    return function () { return; };
+}
+exports.IsDateTime = IsDateTime;
+function IsString(errorMsg) {
+    return function () { return; };
+}
+exports.IsString = IsString;
+function IsArray(errorMsg) {
+    return function () { return; };
+}
+exports.IsArray = IsArray;
+function IsBoolean(errorMsg) {
+    return function () { return; };
+}
+exports.IsBoolean = IsBoolean;
+//# sourceMappingURL=data-types.js.map
